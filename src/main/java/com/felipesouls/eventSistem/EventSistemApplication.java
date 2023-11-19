63b5cdf89e57050d8740d46c63166a3aabcc7a82
Felipe Sousa <felipe.fps09@hotmail.com>
2023-11-18 21:26:53 -0300

    Initialize project

---
+++ src/main/java/com/felipesouls/eventSistem/EventSistemApplication.java
@@ -0,0 +1,13 @@
+package com.felipesouls.eventSistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSistemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipesouls/eventSistem/EventSistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesouls.eventSistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
